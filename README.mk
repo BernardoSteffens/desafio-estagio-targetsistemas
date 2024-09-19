5fb4704ab33fb8176323186006d31252342bff94
bernardo <85709693+BernardoSteffens@users.noreply.github.com>
2024-09-19 17:40:07 -0300

    Adicionando README

--- README.mk
+++ README.mk
@@ -1,8 +1,28 @@
-Questão 4:
-a) 9
-b)128
-c)49
-d) 100
-e) 13
-f) 20
+##Questão 1:
+	Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores
+	(exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número,
+	ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
+	[Solução](github.com/BernardoSteffens/desafio-estagio-targetsistemas/blob/master/src/Fibonacci.java)
 
+##Questão 2
+	Escreva um programa que verifique, em uma string, a existência da letra ‘a’, seja maiúscula ou minúscula, além de informar a quantidade de vezes em que ela ocorre.
+	[Solução](https://github.com/BernardoSteffens/desafio-estagio-targetsistemas/blob/master/src/VerifiqueA.java)
+
+##Questão 3
+	Observe o trecho de código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
+	Ao final do processamento, qual será o valor da variável SOMA?
+
+	Resposta: 77
+	Codígo em java: [Codigo](https://github.com/BernardoSteffens/desafio-estagio-targetsistemas/blob/master/src/TesteCodigo.java)
+
+##Questão 4:
+	Descubra a lógica e complete o próximo elemento:
+	a) 1, 3, 5, 7, ___ Resposta: 9
+	b) 2, 4, 8, 16, 32, 64, ____ Resposta: 128
+	c) 0, 1, 4, 9, 16, 25, 36, ____ Resposta: 49
+	d) 4, 16, 36, 64, ____ Resposta: 100
+	e) 1, 1, 2, 3, 5, 8, ____ Resposta: 13
+	f) 2,10, 12, 16, 17, 18, 19, ____ Resposta: 20
+
+##Questão 5:
+	Não conseguir pensar em uma solução
--- src/TesteCodigo.java
+++ src/TesteCodigo.java
@@ -1,4 +1,4 @@
-public class testeCodigo {
+public class TesteCodigo {
 
     public static void main(String[] args) {
 
